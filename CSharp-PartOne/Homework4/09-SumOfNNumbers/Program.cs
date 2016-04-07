8fe0e91cc994559862ceaf5b6d2fbc0d2965a3db
J0hnyBG <wildman666@gmail.com>
2016-04-07 17:26:17 +0300

    .

---
+++ CSharp-PartOne/Homework4/09-SumOfNNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double[] numbers = new double[n];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = double.Parse(Console.ReadLine());
+        }
+        
+    }
+}
+
