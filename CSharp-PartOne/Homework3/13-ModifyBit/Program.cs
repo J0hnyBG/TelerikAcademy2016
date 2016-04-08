93eb6d579d084351f1aa3075f14f0ce00c536033
J0hnyBG <wildman666@gmail.com>
2016-04-08 23:49:05 +0300

    New tasks

--- CSharp-PartOne/Homework3/13-ModifyBit/Program.cs
+++ CSharp-PartOne/Homework3/13-ModifyBit/Program.cs
@@ -9,14 +9,15 @@ namespace _13_ModifyBit
             ulong n = ulong.Parse(Console.ReadLine());
             byte p = byte.Parse(Console.ReadLine());
             ushort v = ushort.Parse(Console.ReadLine());
+
             ulong result = 0;
             if (v==0)
             {
-                result = n & (ulong)~(1 << p);
+                result = n & ~((ulong)1 << p);
             }
             else if (v == 1)
             {
-                result = n | (ulong)(1 << p);
+                result = n | ((ulong)1 << p);
             }
 
             Console.WriteLine(result);
