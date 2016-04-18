7a52da9a08f9768e5b00dbc75e88a7483580f999
J0hnyBG <wildman666@gmail.com>
2016-04-18 20:36:07 +0300

    new tasks

---
+++ CSharp-PartOne/Exams/Exam-06-2013-v1/Exam-06-2013-v1/04-FireInTheMatrix/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04_FireInTheMatrix
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int width = int.Parse(Console.ReadLine());
+            int offset = 0;
+            int c = 0;
+
+            while (c < 15)
+            {
+                if (c < width / 2)
+                {
+                    offset++;
+                }
+                else if (c > width/2)
+                {
+                    offset--;
+                }
+                for (int i = 1; i <= width; i++)
+                {
+                    if (i == width/2 + offset || i == width/2 + 1 - offset)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+
+                c++;
+            }
+        }
+    }
+}
