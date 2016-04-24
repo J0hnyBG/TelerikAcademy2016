b430dc6d692b68422a55ed0df37c2a89fa03fb99
J0hnyBG <ivan.parvanov1@gmail.com>
2016-04-25 02:55:38 +0300

    added new tasks

--- CSharp-PartOne/Exams/Exam-02-2015-Morning/Exam/03-ConsoleApplication1/Program.cs
+++ CSharp-PartOne/Exams/Exam-02-2015-Morning/Exam/03-ConsoleApplication1/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Numerics;
 
 namespace _03_ConsoleApplication1
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
+            int i = 0;
+            BigInteger result = 1;
+            BigInteger resultGreaterThanTen = 1;
+            //Needed if i>10 and inputStrings are only 0
+            bool result2Modified = false;
 
+            while (true)
+            {
+                BigInteger oddTotalProduct = 1;
+                string inputString = Console.ReadLine();
+                inputString.Trim();
+
+                if (inputString == "END")
+                {
+                    break;
+                }
+
+                if (i%2 == 1)
+                {
+                    foreach (var ch in inputString)
+                    {
+                        if (ch != '0')
+                        {
+                            oddTotalProduct *= (ch - '0');
+                        }
+                    }
+                    if (i <= 10)
+                    {
+                        result *= oddTotalProduct;
+                    }
+                    else
+                    {
+                        result2Modified = true;
+                        resultGreaterThanTen *= oddTotalProduct;
+                    }
+                }
+                i++;
+            }
+            Console.WriteLine(result);
+            if (result2Modified)
+            {
+                Console.WriteLine(resultGreaterThanTen);
+            }
         }
     }
 }
---
+++ CSharp-PartOne/Exams/Exam-02-2015-Morning/Exam/04-PersianRugs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _04_PersianRugs
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            //width= height = 2*n+1
+            int n = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            int width = (2*n) + 1;
+
+            //Top part
+            for (int i = 0; i < (width/2); i++)
+            {
+                DrawLine(width, i, d, true);
+            }
+            //Middle row
+            Console.Write(new string('#', n));
+            Console.Write("X");
+            Console.WriteLine(new string('#', n));
+
+            //Bottom part
+            for (int i = width/2 - 1; i >= 0; i--)
+            {
+                DrawLine(width, i, d, false);
+            }
+        }
+
+        public static void DrawLine(int width, int i, int d, bool drawTop)
+        {
+            char slash1 = '/';
+            char slash2 = '\\';
+
+            if (drawTop)
+            {
+                slash1 = '\\';
+                slash2 = '/';
+            }
+
+            Console.Write(new string('#', i));
+            Console.Write(new string(slash1, 1));
+            if (d >= (width/2) - 1)
+            {
+                Console.Write(new string(' ', width - (2*i) - 2));
+            }
+            else
+            {
+                if (width - (2 * i + 2 * d + 4) > 0)
+                {
+                    Console.Write(new string(' ', d));
+                    Console.Write(new string(slash1, 1));
+                    Console.Write(new string('.', width - (2 * i + 2 * d + 4)));
+                    Console.Write(new string(slash2, 1));
+                    Console.Write(new string(' ', d));
+                }
+                else
+                {
+                   Console.Write(new string(' ', width - (2 * i + 2)));
+                }
+            }
+            Console.Write(new string(slash2, 1));
+            Console.Write(new string('#', i));
+            Console.WriteLine();
+        }
+    }
+}
