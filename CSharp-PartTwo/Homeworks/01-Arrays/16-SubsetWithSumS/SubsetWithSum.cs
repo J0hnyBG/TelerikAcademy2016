8ba4ac18fbc68f7ce1a5197a12141f7e2571bc0a
J0hnyBG <ivan.parvanov1@gmail.com>
2016-05-05 15:14:47 +0300

    added new takss

---
+++ CSharp-PartTwo/Homeworks/01-Arrays/16-SubsetWithSumS/SubsetWithSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _16_SubsetWithSumS
+{
+    internal class SubsetWithSum
+    {
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int sumToFind = int.Parse(Console.ReadLine());
+
+            bool doesSumExist = CheckIfSumExists(arr, 0, sumToFind);
+            Console.WriteLine(doesSumExist ? "yes" : "no");
+        }
+
+        private static bool CheckIfSumExists(int[] arr, int startIndex, int sumToFind)
+        {
+            if (startIndex == arr.Length -1)
+            {
+                return arr[startIndex] == sumToFind || sumToFind == 0;
+            }
+
+            return CheckIfSumExists(arr, startIndex + 1, sumToFind) ||
+                   CheckIfSumExists(arr, startIndex + 1, sumToFind - arr[startIndex]);
+        }
+    }
+}
---
+++ CSharp-PartTwo/Homeworks/01-Arrays/17-SubsetKWitchSumS/SubsetK.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _17_SubsetKWitchSumS
+{
+    class SubsetK
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int sumToFind = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            
+        }
+    }
+}
