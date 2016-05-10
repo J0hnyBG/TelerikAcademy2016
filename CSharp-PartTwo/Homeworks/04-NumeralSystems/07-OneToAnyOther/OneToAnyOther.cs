1e978fb70fcd40128953210ce3ae85a03dfd433c
J0hnyBG <ivan.parvanov1@gmail.com>
2016-05-10 22:25:33 +0300

    added a fix

--- CSharp-PartTwo/Homeworks/04-NumeralSystems/07-OneToAnyOther/OneToAnyOther.cs
+++ CSharp-PartTwo/Homeworks/04-NumeralSystems/07-OneToAnyOther/OneToAnyOther.cs
@@ -2,54 +2,58 @@
 
 namespace _07_OneToAnyOther
 {
-    class OneToAnyOther
-    {
-        static void Main()
+    internal class OneToAnyOther
+    {   //TODO: Fails some tests
+        private static void Main()
         {
-            string inputNumber = Console.ReadLine();
             int startingBase = int.Parse(Console.ReadLine());
+            string inputNumber = Console.ReadLine();
             int targetBase = int.Parse(Console.ReadLine());
-            long numberInDecimal = 0;
 
-            if (startingBase == 10)
-            {
-                numberInDecimal = long.Parse(inputNumber);
-            }
-            else
-            {
-                numberInDecimal = GetNumberInDecimal(inputNumber, startingBase);
-            }
+            long numberInDecimal = GetNumberInDecimal(inputNumber, startingBase);
 
             Console.WriteLine(ConvertToBase(numberInDecimal, targetBase));
         }
 
-        static long GetNumberInDecimal(string input, int startingBase)
+        private static string ConvertToBase(long value, int targetBase)
         {
-            long result = 0;
-            for (int i = 0; i < input.Length; i++)
+            char[] baseChars = GetBaseChars(targetBase);
+            string result = string.Empty;
+
+            do
             {
-                result += (input[input.Length - i - 1] - '0') * (long)Math.Pow(startingBase, i);
-            }
+                result = baseChars[Math.Abs(value)%targetBase] + result;
+                value = value/targetBase;
+            } while (value > 0);
 
             return result;
         }
 
-        static string ConvertToBase(long number, int targetBase)
+        private static char[] GetBaseChars(int targetBase)
         {
-            long result = number;
-            long remainder;
-            string output = string.Empty;
-
-            while (targetBase <= result)
+            //Max base = 16
+            char[] charArray =
             {
-                remainder = result % targetBase;
-                result = result / targetBase;
-                
-                output = remainder + output;
-            }
-            remainder = result % targetBase;
-            output = remainder + output;
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F'
+            };
+            char[] output = new char[targetBase];
+            Array.Copy(charArray, output, targetBase);
+
             return output;
+        }
+
+        private static long GetNumberInDecimal(string input, int startingBase)
+        {
+            char[] baseChars = GetBaseChars(startingBase);
+            long result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                result += (Array.IndexOf(baseChars, input[input.Length - i - 1]))*(long) Math.Pow(startingBase, i);
+            }
+
+            return result;
         }
     }
 }
