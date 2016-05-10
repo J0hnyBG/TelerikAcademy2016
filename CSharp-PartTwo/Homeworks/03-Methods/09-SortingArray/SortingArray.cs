8d78064a0eb45bbb6c50e95d2245ad6aa86c1071
J0hnyBG <ivan.parvanov1@gmail.com>
2016-05-10 20:06:33 +0300

    added new tasks

--- CSharp-PartTwo/Homeworks/03-Methods/09-SortingArray/SortingArray.cs
+++ CSharp-PartTwo/Homeworks/03-Methods/09-SortingArray/SortingArray.cs
@@ -1,11 +1,38 @@
 ﻿using System;
+using System.Linq;
 
 namespace _09_SortingArray
 {
     class SortingArray
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string size = Console.ReadLine();
+            int[] arr = Console.ReadLine().Trim().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+            //Array.Sort(arr);
+            BubbleSort(arr);
+            foreach (var number in arr)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+        static void BubbleSort(int[] arr)
+        {
+            int temp = 0;
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
         }
     }
 }
---
+++ CSharp-PartTwo/Homeworks/03-Methods/10-NFactorial/nFactorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace _10_NFactorial
+{
+    class nFactorial
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger result = GetFactorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        static BigInteger GetFactorial(int n)
+        {
+            BigInteger factorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
