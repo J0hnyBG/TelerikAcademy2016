a2dbbaf19a545a04af13703ddd1c8f05cf086a59
J0hnyBG <ivan.parvanov1@gmail.com>
2016-05-19 14:26:46 +0300

    added new tasks

--- CSharp-PartTwo/Exams/2015-03-05-Evening/04-Konspiration/Konspiration.cs
+++ CSharp-PartTwo/Exams/2015-03-05-Evening/04-Konspiration/Konspiration.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 
 namespace _04_Konspiration
 {
@@ -6,6 +9,31 @@ namespace _04_Konspiration
     {
         static void Main()
         {
+			//Regex to match static method names [Ss]tatic +\w+ +(\w+)
+			// Regex to match static method invocation (\w+) *\(.*\)
+			//Regex to match method invocation (\.[\s\n\r]*[\w]+)[\s\n\r]*(?=\(.*\))
+			//Mother of regexes for methods (?!\bif\b|\bfor\b|\bwhile\b|\bswitch\b|\btry\b|\bcatch\b)(\b[\w]+\b)[\s\n\r]*(?=\(.*\))
+
+            Regex regex = new Regex(@"(?!\bif\b|\bfor\b|\bwhile\b|\bswitch\b|\btry\b|\bcatch\b)(\b[\w]+\b)[\s\n\r]*(?=\(.*\))");
+            List<string> listOfMethods = new List<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            string line = "";
+            for (int i = 0; i < n; i++)
+            {
+                line = line + Console.ReadLine();
+            }
+
+            foreach ( Match match in regex.Matches(line) )
+            {
+                listOfMethods.Add(match.Value);
+            }
+
+            foreach (var methodName in listOfMethods)
+            {
+                Console.WriteLine(methodName);
+            }
+
 
         }
     }
