d5b9aae610500d1d04207292c50f7b787e2c9c12
J0hnyBG <ivan.parvanov1@gmail.com>
2016-05-20 17:30:43 +0300

    new tasks

--- CSharp-PartTwo/Homeworks/06-StringsAndTextProcessing/05-ParseTags/ParseTags.cs
+++ CSharp-PartTwo/Homeworks/06-StringsAndTextProcessing/05-ParseTags/ParseTags.cs
@@ -2,17 +2,25 @@
 
 namespace _05_ParseTags
 {
-    class ParseTags
+    internal class ParseTags
     {
         private static string _inputString = string.Empty;
-        static void Main()
+        private const string _openingTag = "<uppercase>";
+        private const string _closingTag = "</uppercase>";
+
+        private static void Main()
         {
             _inputString = Console.ReadLine();
+
+            int startIndex = GetIndexOfInner(_inputString, _openingTag) + _openingTag.Length;
+            int endIndex = GetIndexOfInner(_inputString, _closingTag);
+
         }
 
-        static void TagParse()
+        private static int GetIndexOfInner(string text, string tag)
         {
-            
+            int index = text.IndexOf(tag);
+            return index;
         }
     }
 }
