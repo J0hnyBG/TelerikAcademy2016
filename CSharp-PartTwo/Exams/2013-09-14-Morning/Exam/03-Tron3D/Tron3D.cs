0dd2c6e8db5514f21cd4a90f3d07e52acef23526
J0hnyBG <ivan.parvanov1@gmail.com>
2016-05-30 14:24:54 +0300

    .

--- CSharp-PartTwo/Exams/2013-09-14-Morning/Exam/03-Tron3D/Tron3D.cs
+++ CSharp-PartTwo/Exams/2013-09-14-Morning/Exam/03-Tron3D/Tron3D.cs
@@ -3,33 +3,208 @@ using System.Linq;
 
 internal class Tron3D
 {
-    private static bool[,] VisitedCells;
+    private static bool[,] used;
+    private static int oldX, oldY, oldZ;
+    private static int X, Y;
+    static string redMoves, blueMoves;
+    private static Player BluePlayer;
+    private static Player RedPlayer;
 
     private static void Main()
     {
-        int[] dimensions = Console.ReadLine().Split(' ').Select(n => int.Parse(n)).ToArray();
-        int x = dimensions[0];
-        int y = dimensions[1];
-        int z = dimensions[2];
-        VisitedCells = new bool[2 * x + 2 * z, y];
+        ReadInput();
+        X = oldX;
+        Y = oldY + oldZ + oldY + oldZ;
+        used = new bool[X + 1, Y];
+
+        RedPlayer = new Player(oldX/2, oldY/2, 0, redMoves);
+        BluePlayer = new Player(oldX/2, oldY / 2 + oldZ + oldY, 2, blueMoves);
+
+        while (true)
+        {
+            while (RedPlayer.Index < RedPlayer.Moves.Length && RedPlayer.Moves[RedPlayer.Index] != 'M')
+            {
+                RotatePlayer(RedPlayer);
+                RedPlayer.Index++;
+            }
+            RedPlayer.Move();
+            RedPlayer.Index++;
+            
+            if ( RedPlayer.PosY >= Y )
+            {
+                RedPlayer.PosY = 0;
+            }
+            if ( RedPlayer.PosY < 0 )
+            {
+                RedPlayer.PosY = Y - 1;
+            }
+
+            while ( BluePlayer.Index < BluePlayer.Moves.Length && BluePlayer.Moves[BluePlayer.Index] != 'M' )
+            {
+                RotatePlayer(BluePlayer);
+                BluePlayer.Index++;
+            }
+            BluePlayer.Move();
+            BluePlayer.Index++;
+            if ( BluePlayer.PosY >= Y )
+            {
+                BluePlayer.PosY = 0;
+            }
+            if ( BluePlayer.PosY < 0 )
+            {
+                BluePlayer.PosY = Y - 1;
+            }
+
+            bool redLooses = Looses(RedPlayer);
+            bool blueLooses = Looses(BluePlayer);
+
+            if (redLooses || blueLooses)
+            {
+                //TODO: Calculate distance between crash and start
+                if ( redLooses && blueLooses )
+                {
+                    Console.WriteLine("DRAW");
+                    //RedPlayer.MoveBack();
+                    Console.WriteLine(GetDistanceFromStart(RedPlayer.PosX, RedPlayer.PosY, oldX, oldY, oldZ) - 1);
+
+                }
+                else if ( redLooses )
+                {
+                    Console.WriteLine("BLUE");
+                    Console.WriteLine(GetDistanceFromStart(BluePlayer.PosX, BluePlayer.PosY, oldX, oldY, oldZ));
+
+                }
+                else if ( blueLooses )
+                {
+                    Console.WriteLine("RED");
+                    Console.WriteLine(GetDistanceFromStart(RedPlayer.PosX, RedPlayer.PosY, oldX, oldY, oldZ));
+                }
+
+                break;
+            }
+            used[RedPlayer.PosX, RedPlayer.PosY] = true;
+            used[BluePlayer.PosX, BluePlayer.PosY] = true;
+        }
     }
 
-    private static void Initialize(int x, int y, int z)
+    static double GetDistanceFromStart(int h, int v, int x, int y, int z)
     {
+        if ( h > z + y + y / 2 )
+            h = 2 * z + y - h;
+
+        return Math.Abs(h - y / 2) + Math.Abs(v - x / 2);
+    }
+
+    public static bool Looses(Player player)
+    {
+        if (player.PosX < 0 || player.PosX > X)
+        {
+            return true;
+        }
+        return used[player.PosX, player.PosY];
+    }
+
+    static void RotatePlayer(Player player)
+    {
+        switch ( player.Moves[player.Index] )
+        {
+            case 'L':
+                player.RotateLeft();
+                break;
+            case 'R':
+                player.RotateRight();
+                break;
+            default:
+                throw new ArgumentException("The move is invalid.");
+        }
+    }
+    public static void ReadInput()
+    {
+        var dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        oldX = dimensions[0];
+        oldY = dimensions[1];
+        oldZ = dimensions[2];
+        redMoves = Console.ReadLine();
+        blueMoves = Console.ReadLine();
     }
 }
 
-internal class Player
+class Player
 {
-    private bool[,] VisitedCells;
+    public int PosX { get; set; }
+    public int PosY { get; set; }
+    public int Direction { get; set; }
+    public int Index { get; set; }
+    public string Moves { get; set; }
 
-    public Player(bool[,] visitedCells, int posX, int posY)
+    public Player(int posX, int posY, int direction, string moves, int index = 0)
     {
-        //VisitedCells = new bool[,];
         PosX = posX;
         PosY = posY;
+        Direction = direction;
+        Index = index;
+        Moves = moves;
     }
 
-    private int PosX;
-    private int PosY;
+    //Directions:
+    //0 -> right
+    //1 -> down
+    //2 -> left
+    //3 -> up
+    //4 -> 0
+    //rotate right -> direction++;
+    //rotate left -> direction--;
+   public void Move()
+    {
+        switch (Direction)
+        {
+            case 0:
+                PosY++;
+                break;
+            case 1:
+                PosX++;
+                break;
+            case 2:
+                PosY--;
+                break;
+            case 3:
+                PosX--;
+                break;
+        }
+    }
+    public void MoveBack()
+    {
+        switch ( Direction )
+        {
+            case 0:
+                PosY--;
+                break;
+            case 1:
+                PosX--;
+                break;
+            case 2:
+                PosY++;
+                break;
+            case 3:
+                PosX++;
+                break;
+        }
+    }
+
+    public void RotateLeft()
+    {
+        Direction--;
+        if (Direction < 0)
+        {
+            Direction = 3;
+        }
+    }
+    public void RotateRight()
+    {
+        Direction++;
+        if ( Direction > 3 )
+        {
+            Direction = 0;
+        }
+    }
 }
