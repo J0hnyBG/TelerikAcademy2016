e3a8e4c55083a57d7ca5c3fc7e135c10e8c97bed
J0hnyBG <ivan.parvanov1@gmail.com>
2017-03-19 16:31:30 +0200

    added new tasks

---
+++ ASP.NET-MVC/Homeworks/06-Caching/06-Caching/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace _06_Caching
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NET-MVC/Homeworks/06-Caching/06-Caching/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace _06_Caching.Controllers
+{
+    public class HomeController : Controller
+    {
+        [OutputCache(Duration = 3600)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        [OutputCache(Duration = 600)]
+        public ActionResult PartialAbout()
+        {
+            return this.PartialView("_About");
+        }
+
+        public ActionResult Contact()
+        {
+            if (this.HttpContext.Cache["files"] == null)
+            {
+                var files = Directory.GetFiles("C:\\");
+
+                this.HttpContext.Cache.Add("files", files, new CacheDependency("C:\\"), DateTime.UtcNow.AddMinutes(60), Cache.NoSlidingExpiration, CacheItemPriority.Default, null);
+            }
+
+            return this.View(this.HttpContext.Cache["files"]);
+        }
+    }
+}
